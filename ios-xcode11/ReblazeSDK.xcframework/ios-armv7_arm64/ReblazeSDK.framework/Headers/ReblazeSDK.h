06b872d09e78a6b72a19bbec4042b15cf0821d5a
guydoron <guy@moblers.com>
2019-10-07 15:08:10 +0300

    added xcode11 support folder

---
+++ ios-xcode11/ReblazeSDK.xcframework/ios-armv7_arm64/ReblazeSDK.framework/Headers/ReblazeSDK.h
@@ -0,0 +1,20 @@
+//
+//  ReblazeSDK.h
+//  ReblazeSDK
+//
+//  Created by Rotem Doron on 26/09/2019.
+//  Copyright © 2019 Moblers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <ReblazeSDK/Hasher.h>
+
+//! Project version number for ReblazeSDK.
+FOUNDATION_EXPORT double ReblazeSDKVersionNumber;
+
+//! Project version string for ReblazeSDK.
+FOUNDATION_EXPORT const unsigned char ReblazeSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReblazeSDK/PublicHeader.h>
+
+
