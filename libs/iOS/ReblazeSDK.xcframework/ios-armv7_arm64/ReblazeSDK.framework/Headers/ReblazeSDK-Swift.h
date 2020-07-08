3ee8ed1046d246c6e1f5b5189f924215b37e65e4
ksenia <ksenia@bekitzur.com>
2020-07-08 19:11:02 +0300

    updated ios version to 1.7.1

--- libs/iOS/ReblazeSDK.xcframework/ios-armv7_arm64/ReblazeSDK.framework/Headers/ReblazeSDK-Swift.h
+++ libs/iOS/ReblazeSDK.xcframework/ios-armv7_arm64/ReblazeSDK.framework/Headers/ReblazeSDK-Swift.h
@@ -237,6 +237,29 @@ SWIFT_CLASS("_TtC10ReblazeSDK7Reblaze")
 /// *
 ///
 /// \endcode
++ (BOOL)startWith:(NSString * _Nonnull)address secret:(NSString * _Nonnull)secret key:(NSString * _Nonnull)key uid:(NSString * _Nonnull)uid shouldShowLogs:(BOOL)shouldShowLogs intervalInSeconds:(NSInteger)intervalInSeconds reportLocation:(BOOL)reportLocation error:(NSError * _Nullable * _Nullable)error;
+/// Main starting point for starting SDK
+/// \code
+///   - Parameters:
+///       - address : URL for the customer's application server
+///       - secret : Secret value issued to the customer by Reblaze
+///       - key : The name of a field that contains a unique value for this user. (Example: UserName.)
+///       - uid : The value for the field specified above. (For example, the user's email address.)
+///       - shouldShowLogs : Value indicating will logs be printed in debug console
+///       - intervalInSeconds : interval in seconds when the events will be sent. Must be in range 12...300 seconds. If value specified to -1, this means no events will be send authomatically
+///       - userAgent: custom user agent will be send in requests (optional)
+///       - reportLocation: value indicating will location data be reported or not
+///
+///
+///
+///    - Throws: An error of type `StartError` or  `TimerError`
+///
+/// \endcode<ul>
+///   <li>
+///   </li>
+///   <li>
+///   </li>
+/// </ul>
 + (BOOL)startWith:(NSString * _Nonnull)address secret:(NSString * _Nonnull)secret key:(NSString * _Nonnull)key uid:(NSString * _Nonnull)uid shouldShowLogs:(BOOL)shouldShowLogs intervalInSeconds:(NSInteger)intervalInSeconds userAgent:(NSString * _Nullable)userAgent reportLocation:(BOOL)reportLocation error:(NSError * _Nullable * _Nullable)error;
 /// Calculates hash based on timestamp, secret and unique value for this user
 /// \code
@@ -507,6 +530,29 @@ SWIFT_CLASS("_TtC10ReblazeSDK7Reblaze")
 /// *
 ///
 /// \endcode
++ (BOOL)startWith:(NSString * _Nonnull)address secret:(NSString * _Nonnull)secret key:(NSString * _Nonnull)key uid:(NSString * _Nonnull)uid shouldShowLogs:(BOOL)shouldShowLogs intervalInSeconds:(NSInteger)intervalInSeconds reportLocation:(BOOL)reportLocation error:(NSError * _Nullable * _Nullable)error;
+/// Main starting point for starting SDK
+/// \code
+///   - Parameters:
+///       - address : URL for the customer's application server
+///       - secret : Secret value issued to the customer by Reblaze
+///       - key : The name of a field that contains a unique value for this user. (Example: UserName.)
+///       - uid : The value for the field specified above. (For example, the user's email address.)
+///       - shouldShowLogs : Value indicating will logs be printed in debug console
+///       - intervalInSeconds : interval in seconds when the events will be sent. Must be in range 12...300 seconds. If value specified to -1, this means no events will be send authomatically
+///       - userAgent: custom user agent will be send in requests (optional)
+///       - reportLocation: value indicating will location data be reported or not
+///
+///
+///
+///    - Throws: An error of type `StartError` or  `TimerError`
+///
+/// \endcode<ul>
+///   <li>
+///   </li>
+///   <li>
+///   </li>
+/// </ul>
 + (BOOL)startWith:(NSString * _Nonnull)address secret:(NSString * _Nonnull)secret key:(NSString * _Nonnull)key uid:(NSString * _Nonnull)uid shouldShowLogs:(BOOL)shouldShowLogs intervalInSeconds:(NSInteger)intervalInSeconds userAgent:(NSString * _Nullable)userAgent reportLocation:(BOOL)reportLocation error:(NSError * _Nullable * _Nullable)error;
 /// Calculates hash based on timestamp, secret and unique value for this user
 /// \code
