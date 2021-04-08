5979a3698bbd30e46ea5db588877f23858ab44a6
Alex Cohn <alexcohn@reblaze.com>
2021-04-08 15:11:36 +0300

    cleanup iOS examples

--- examples/ios-xcode11/Pods/Reblaze/README.md
+++
@@ -1,8 +0,0 @@
-Release notes are available here:
-
-https://bitbucket.org/reblaze/mobilesdk/src/master/release-notes.md
-
-
-Complete documentation for the Reblaze SDK is available here:
-
-https://gb.docs.reblaze.com/v/mobile-sdk/
--- examples/ios-xcode11/Pods/Reblaze/libs/iOS/ReblazeSDK.xcframework/ios-arm64_armv7/ReblazeSDK.framework/Headers/ReblazeSDK.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <ReblazeSDK/Hasher.h>
-
-//! Project version number for ReblazeSDK.
-FOUNDATION_EXPORT double ReblazeSDKVersionNumber;
-
-// In this header, you should import all the public headers of your framework using statements like #import <ReblazeSDK/PublicHeader.h>
-
-
