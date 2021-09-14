0e691fb45bb25be4a043603aaf6a0db5f1a1cc21
Alex Cohn <alexcohn@reblaze.com>
2021-09-14 19:37:36 +0300

    add v2.0 (debug) binaries

---
+++ libs/iOS/ReblazeSDK.xcframework/ios-arm64_i386_x86_64-simulator/ReblazeSDK.framework/Headers/ReblazeSDK.h
@@ -0,0 +1,20 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for ReblazeSDK.
+FOUNDATION_EXPORT const double ReblazeSDKVersionNumber;
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReblazeSDK/PublicHeader.h>
+
+const static char *ReblazeSDK_KindName[] = {
+    "VERBOSE",
+    "DEBUG",
+    "INFO",
+    "WARN",
+    "ERROR"};
+
+
+
+
+
+
+
