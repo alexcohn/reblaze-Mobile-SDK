82174cce3b81187411a238b8c0f903b44ac1c87d
Alex Cohn <alexcohn@reblaze.com>
2021-10-12 23:59:19 +0300

    use reblaze wrapper in main.dart

--- examples/flutter/lib/main.dart
+++ examples/flutter/lib/main.dart
@@ -1,15 +1,24 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:html/parser.dart' show parse;
+
+import 'reblaze.dart';
 
 void main() {
   runApp(ReblazeExampleApp());
 }
 
+final client = http.Client();
+
 Future<String> fetch(String path) async {
-  final response = await http.get(Uri.parse('https://fire-gcp-lb.rbzdevsdk001.dev.rbzdns.com/flutter/$path'),
+  final response = await client.get('${await reblaze.getBackendUrl()}/flutter/$path',
       headers: {
-        'rbzid': 'test',
+        HttpHeaders.acceptHeader: "text/html",                     // to get ISHUMAN header back from our echo server
+        HttpHeaders.authorizationHeader: "alex",                   // must be same as reblaze.getToken()
+        HttpHeaders.userAgentHeader: await reblaze.getUserAgent(), // better keep it same as the UA that the SDK sends
+        reblaze.reblazeHeader: await reblaze.generateHash(),       // must be generated for every request
       }
   );
   if (response.statusCode == 200) {
@@ -30,6 +39,10 @@ Future<String> fetch(String path) async {
 class ReblazeExampleApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    reblaze.setInterval(30);
+    reblaze.setToken('alex');
+    reblaze.setTimeslice(1000);
+    reblaze.setBackendUrl('https://fire-gcp-lb.rbzdevsdk001.dev.rbzdns.com');
     return MaterialApp(
       title: 'Reblaze Example',
       home: ReblazeExamplePage(title: 'Reblaze Example Page'),
@@ -54,12 +67,14 @@ class _ReblazeExamplePageState extends State<ReblazeExamplePage> {
   void initState() {
     super.initState();
     futureResponse = fetch('init');
+    reblaze.getBackendUrl().then( (backendUrl) => print('BackendUrl: $backendUrl') );
   }
 
   void _incrementCounter() {
     setState(() {
       _counter++;
       futureResponse = fetch(_counter.toString());
+      reblaze.sendEvent('counter');
     });
   }
 
