e09e9746e914241c3f32630d4dcd24ff581028bd
Alex Cohn <alexcohn@reblaze.com>
2021-10-13 00:14:45 +0300

    use the SDK adapter for flutter

--- examples/flutter/android/app/src/main/java/com/example/reblaze_flutter/MainActivity.java
+++ examples/flutter/android/app/src/main/java/com/example/reblaze_flutter/MainActivity.java
@@ -11,68 +11,6 @@ public class MainActivity extends FlutterActivity {
     @Override
     public void configureFlutterEngine(@NonNull io.flutter.embedding.engine.FlutterEngine flutterEngine) {
         super.configureFlutterEngine(flutterEngine);
-
-        new io.flutter.plugin.common.MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), "reblaze.com").setMethodCallHandler(
-                (methodCall, result) -> {
-                  switch (methodCall.method) {
-                    case "generateHash":
-                        result.success(reblaze.generateHash());
-                        return;
-                    case "sendEvent":
-                        String eventName = methodCall.arguments();
-                        reblaze.sendEvent(eventName);
-                        return;
-                    case "setBackendUrl":
-                        String backendUrl = methodCall.arguments();
-                        reblaze.setBackendUrl(backendUrl);
-                        return;
-                    case "getBackendUrl":
-                        result.success(reblaze.getBackendUrl());
-                        return;
-                    case "getUserAgent":
-                        result.success(reblaze.getUserAgent());
-                        return;
-                    case "getConfigurationName":
-                        result.success(reblaze.getConfigurationName());
-                        return;
-                    case "getHashVariant":
-                        result.success(reblaze.getHashVariant());
-                        return;
-                    case "getToken":
-                        result.success(reblaze.getToken());
-                        return;
-                    case "getTokenHeaderName":
-                        result.success(reblaze.getTokenHeaderName());
-                        return;
-                    case "getTimeslice":
-                        result.success(reblaze.getTimeslice());
-                        return;
-                    case "getInterval":
-                        result.success(reblaze.getInterval());
-                        return;
-                    case "getReportCounters":
-                        result.success(reblaze.getReportCounters());
-                        return;
-                    case "setToken":
-                        reblaze.setToken(methodCall.arguments());
-                        return;
-                    case "setTokenHeaderName":
-                        reblaze.setTokenHeaderName(methodCall.arguments());
-                        return;
-                    case "setTimeslice":
-                        reblaze.setTimeslice(methodCall.arguments());
-                        return;
-                    case "setInterval":
-                        Object qq = methodCall.arguments();
-                        reblaze.setInterval(methodCall.arguments());
-                        return;
-                    case "setReportCounters":
-                        reblaze.setReportCounters(methodCall.arguments());
-                        return;
-                    default:
-                        result.notImplemented();
-                }
-            }
-        );
+        reblaze.registerFlutterMethodChannel(flutterEngine);
     };
 };
