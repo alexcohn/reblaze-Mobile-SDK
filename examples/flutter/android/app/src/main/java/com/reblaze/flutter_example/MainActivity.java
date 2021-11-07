716f164d6111a4a19566828ab155e3e97999c412
Alex Cohn <alexcohn@reblaze.com>
2021-11-07 23:05:28 +0200

    rename the flutter app package for Android

---
+++ examples/flutter/android/app/src/main/java/com/reblaze/flutter_example/MainActivity.java
@@ -0,0 +1,11 @@
+package com.reblaze.flutter_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+
+public class MainActivity extends FlutterActivity {
+
+    public MainActivity() {
+        super();
+    }
+}
